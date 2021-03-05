b620b6e2b6b221a15387d073be3cf06680e8a356
Maykol Rey <maykolrey@gmail.com>
2021-03-05 08:22:01 -0500

    extration link of page

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-html="res3"></div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      links:[],
+      res3: ""
+    };
+  },
+
+  async created(){
+      const headers = { "Content-Type": "text/html" };
+      const myRequest = new Request('http://dl.icdst.org/pdfs/files/');
+
+      const res = await fetch(myRequest,{headers})
+      
+      this.res3=await res.text()
+      this.links = await document.getElementsByTagName("a") 
+
+
+      
+      
+ 
+  }
+
+}
+</script>
